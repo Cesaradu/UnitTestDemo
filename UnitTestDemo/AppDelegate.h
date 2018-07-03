8071627b50377c41522be6c7e839ff2b54698a93
César <424321999@qq.com>
2018-07-03 15:08:07 +0800

    Initial Commit

---
+++ UnitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitTestDemo
+//
+//  Created by Oliver on 2018/7/3.
+//  Copyright © 2018年 Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitTestDemo
+//
+//  Created by Oliver on 2018/7/3.
+//  Copyright © 2018年 Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
